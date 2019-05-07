3801fd5360a7e4ba2110d565deb6084daaaa6b62
kmj4473 <zzzits4473@naver.com>
2019-05-07 15:37:54 +0900

    add javadoc sample

--- src/test/java/com/kmj/calculator/CalculatorTest.java
+++ src/test/java/com/kmj/calculator/CalculatorTest.java
@@ -1,6 +1,11 @@
 package com.kmj.calculator;
+
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+/**
+*
+*/
 public class CalculatorTest {
 private Calculator calculator = new Calculator();
 @Test
